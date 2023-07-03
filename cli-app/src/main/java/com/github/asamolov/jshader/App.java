3e787a4ed64f9c0e3de63a5e8ea64e4226361939
Alexander Samolov <alexsamolov@gmail.com>
2023-07-03 19:36:20 +0200

    Greeter lib + CLI app

---
+++ cli-app/src/main/java/com/github/asamolov/jshader/App.java
@@ -0,0 +1,16 @@
+package com.github.asamolov.jshader;
+
+import com.github.asamolov.jshader.lib.Greeter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+	var greeter = new Greeter();
+	var user = System.getProperty("user.name");
+        System.out.println(greeter.greet(user));
+    }
+}
+
---
+++ lib/src/main/java/com/github/asamolov/jshader/lib/Greeter.java
@@ -0,0 +1,12 @@
+package com.github.asamolov.jshader.lib;
+
+import java.util.Optional;
+
+/**
+ * Hello world!
+ */
+public class Greeter {
+    public String greet(String name) {	
+        return String.format("Howdy, %s!", Optional.ofNullable(name).orElse("%username%"));
+    }
+}
